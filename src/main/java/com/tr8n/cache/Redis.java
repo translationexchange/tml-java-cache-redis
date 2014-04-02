9f4f7f5cf5a17cbfb4d67f8bf6ce92e856ec5924
Michael Berkovich <theiceberk@gmail.com>
2014-04-01 22:24:02 -0700

    fixed redis support

--- src/main/java/com/tr8n/cache/Redis.java
+++ src/main/java/com/tr8n/cache/Redis.java
@@ -1,32 +1,53 @@
+/*
+ *  Copyright (c) 2014 Michael Berkovich, http://tr8nhub.com All rights reserved.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
 package com.tr8n.cache;
 
 import java.util.Map;
 
-import net.spy.memcached.AddrUtil;
-import net.spy.memcached.MemcachedClient;
+import redis.clients.jedis.Jedis;
 
 import com.tr8n.core.Cache;
 import com.tr8n.core.Tr8n;
 
 public class Redis extends Cache {
-	MemcachedClient client;
+	Jedis jedis;
 	Integer version;
 	
 	public Redis(Map<String, Object> config) {
 		super(config);
 	}
 
-	private MemcachedClient getMemcachedClient() throws Exception {
-		if (client == null) {
-			client = new MemcachedClient(AddrUtil.getAddresses((String)getConfig().get("host")));
+	private Jedis getJedis() {
+		if (jedis == null) {
+			jedis = new Jedis((String)getConfig().get("host"));
 		}
 		
-		return client;
+		return jedis;
 	}
 
 	public Integer getVersion() {
 		try {
-			version = (Integer) getMemcachedClient().get("version");
+			version = new Integer(getJedis().get("version"));
 			if (version == null) {
 				version = (Integer) getConfig().get("version");
 				setVersion(version);
@@ -40,7 +61,7 @@ public class Redis extends Cache {
 	
 	public void setVersion(Integer version) {
 		try {
-			getMemcachedClient().set("version", 0, version);
+			getJedis().set("version", version.toString());
 			this.version = version;
 		} catch (Exception ex) {
 		}
@@ -65,7 +86,7 @@ public class Redis extends Cache {
 		if (isInlineMode(options)) return null;
 		
 		try {
-			return getMemcachedClient().get(getVersionedKey(key));
+			return getJedis().get(getVersionedKey(key));
 		} catch (Exception ex) {
 			Tr8n.getLogger().logException("Failed to get a value from Memcached", ex);
 			return null;
@@ -77,7 +98,7 @@ public class Redis extends Cache {
 		if (isInlineMode(options)) return;
 
 		try {
-			getMemcachedClient().set(getVersionedKey(key), getTimeout(), data);
+			getJedis().set(getVersionedKey(key), data.toString());
 		} catch (Exception ex) {
 			Tr8n.getLogger().logException("Failed to store a value in Memcached", ex);
 		}
@@ -86,7 +107,7 @@ public class Redis extends Cache {
 	@Override
 	public void delete(String key, Map<String, Object> options) {
 		try {
-			getMemcachedClient().delete(getVersionedKey(key));
+			getJedis().set(getVersionedKey(key), null);
 		} catch (Exception ex) {
 			Tr8n.getLogger().logException("Failed to delete a value from Memcached", ex);
 		}
